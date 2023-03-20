1537e958e26b664afc40adef2395b00e6b1e4a14
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-20 18:19:16 +0300

    task 21

---
+++ tests/Task21Test.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Model\ListNode;
+use App\Tasks\Task21;
+use PHPUnit\Framework\TestCase;
+
+final class Task21Test extends TestCase
+{
+    public function test21(): void
+    {
+        $sut = new Task21();
+
+        $list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        $list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        $result = $sut->mergeTwoLists($list1, $list2);
+        self::assertSame($result?->toArray(), [1, 1, 2, 3, 4, 4]);
+
+        $result = $sut->mergeTwoLists(null, null);
+        self::assertSame($result, null);
+
+        $result = $sut->mergeTwoLists(null, new ListNode(0));
+        self::assertSame($result?->toArray(), [0]);
+    }
+}
