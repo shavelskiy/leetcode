32b658a579b98a658a985da9ae844c842ac4b9ad
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-21 14:47:55 +0300

    task 322

---
+++ src/Tasks/Task322.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/coin-change/.
+ * 51.52/72.3.
+ */
+final class Task322
+{
+    /**
+     * @param int[] $coins
+     */
+    public function coinChange(array $coins, int $amount): int
+    {
+        /** @var int[] */
+        $map = [0];
+
+        for ($i = 1; $i <= $amount; $i++) {
+            $map[$i] = null;
+            foreach ($coins as $coin) {
+                $prevValue = $map[$i - $coin] ?? null;
+                if ($prevValue === null) {
+                    continue;
+                }
+
+                $map[$i] = $map[$i] === null ? ($prevValue + 1) : min($map[$i], $prevValue + 1);
+            }
+        }
+
+        return $map[$amount] ?? -1;
+    }
+}
---
+++ tests/Task322Test.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task322;
+use PHPUnit\Framework\TestCase;
+
+final class Task322Test extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     *
+     * @param int[] $coins
+     */
+    public function test322(array $coins, int $amount, int $result): void
+    {
+        $sut = new Task322();
+
+        self::assertSame($sut->coinChange($coins, $amount), $result);
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            [[1, 2, 5], 11, 3],
+            [[2], 3, -1],
+            [[1], 0, 0],
+        ];
+    }
+}
