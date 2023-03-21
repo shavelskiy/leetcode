868a046682d37bc91d66d7593fc02836a756ff52
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-22 00:02:14 +0300

    task 200

---
+++ tests/Task200Test.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task200;
+use PHPUnit\Framework\TestCase;
+
+final class Task200Test extends TestCase
+{
+    public function test200(): void
+    {
+        $sut = new Task200();
+
+        $data = [
+            ['1', '0', '1', '1', '1'],
+            ['1', '0', '1', '0', '1'],
+            ['1', '1', '1', '0', '1'],
+        ];
+
+        self::assertSame($sut->numIslands($data), 1);
+
+        $data = [
+            ['1', '1', '1', '1', '0'],
+            ['1', '1', '0', '1', '0'],
+            ['1', '1', '0', '0', '0'],
+            ['0', '0', '0', '0', '0'],
+        ];
+
+        self::assertSame($sut->numIslands($data), 1);
+
+        $data = [
+            ['1', '1', '0', '0', '0'],
+            ['1', '1', '0', '0', '0'],
+            ['0', '0', '1', '0', '0'],
+            ['0', '0', '0', '1', '1'],
+        ];
+
+        self::assertSame($sut->numIslands($data), 3);
+    }
+}
