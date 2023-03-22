1701a04a85aa6a3e2f3d554c8ab41a6c448b74e9
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-22 21:26:14 +0300

    task 53

---
+++ tests/Task53Test.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+use PHPUnit\Framework\TestCase;
+
+final class Task53Test extends TestCase
+{
+    public function test53(): void
+    {
+        $sut = new Task53();
+
+        self::assertSame($sut->maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
+        self::assertSame($sut->maxSubArray([1]), 1);
+        self::assertSame($sut->maxSubArray([5, 4, -1, 7, 8]), 23);
+    }
+}
