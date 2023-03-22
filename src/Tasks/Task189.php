8108116a27e5120f7453aa67371535dda79f4093
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-22 17:38:44 +0300

    task 189

---
+++ src/Tasks/Task189.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/rotate-array.
+ * 65.45/60.91.
+ */
+final class Task189
+{
+    /**
+     * @param int[] $nums
+     */
+    public function rotate(array &$nums, int $k): void
+    {
+        $n = count($nums);
+        $result = array_fill(0, $n, 0);
+        for ($i = 0; $i < $n; $i++) {
+            $result[($i + $k) % $n] = $nums[$i];
+        }
+
+        $nums = $result;
+    }
+}
