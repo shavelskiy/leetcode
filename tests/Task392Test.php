ba1427dba4d1ab2c2d8da715953bb02bd3d303e5
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-22 19:54:48 +0300

    task 392

---
+++ tests/Task392Test.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task392;
+use PHPUnit\Framework\TestCase;
+
+final class Task392Test extends TestCase
+{
+    public function test392(): void
+    {
+        $sut = new Task392();
+
+        self::assertSame($sut->isSubsequence('', 'ahbgdc'), true);
+        self::assertSame($sut->isSubsequence('abc', 'ahbgdc'), true);
+        self::assertSame($sut->isSubsequence('axc', 'ahbgdc'), false);
+    }
+}
