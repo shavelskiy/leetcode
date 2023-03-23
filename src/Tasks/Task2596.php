ce4cca7c5d24880c99c30816f4b5119f4f030b3a
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-24 01:33:37 +0300

    task 2596 refactoring

--- src/Tasks/Task2596.php
+++ src/Tasks/Task2596.php
@@ -6,7 +6,7 @@ namespace App\Tasks;
 
 /**
  * https://leetcode.com/problems/check-knight-tour-configuration/
- * 75/37.5
+ * 87.5/100
  */
 final class Task2596
 {
@@ -26,21 +26,21 @@ final class Task2596
      */
     public function checkValidGrid(array $grid): bool
     {
-        return $this->solve($grid, 0, 0, 0);
+        return $this->solve($grid, 0, 0, 0, count($grid) * count($grid) - 1);
     }
 
-    private function solve(array $grid, int $i, int $j, int $need): bool
+    private function solve(array $grid, int $i, int $j, int $need, int $max): bool
     {
         if (($grid[$i][$j] ?? 0) !== $need) {
             return false;
         }
 
-        if ($need === count($grid) * count($grid) - 1) {
+        if ($need === $max) {
             return true;
         }
 
         foreach (self::MOVE as $item) {
-            if ($this->solve($grid, $i + $item[0], $j + $item[1], $need + 1) === true) {
+            if ($this->solve($grid, $i + $item[0], $j + $item[1], $need + 1, $max) === true) {
                 return true;
             }
         }
