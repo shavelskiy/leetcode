b39bf5fd5237b44d14e13ba07f7739ba6f69bd8e
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-23 10:18:51 +0300

    task 283

---
+++ src/Tasks/Task283.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/move-zeroes/.
+ */
+final class Task283
+{
+    /**
+     * @param int[] $nums
+     */
+    public function moveZeroes(array &$nums): void
+    {
+        $count = 0;
+        foreach ($nums as $key => $num) {
+            if ($num === 0) {
+                $count++;
+                unset($nums[$key]);
+            }
+        }
+
+        $nums = array_values($nums);
+        for ($i = 0; $i < $count; $i++) {
+            $nums[] = 0;
+        }
+    }
+}
