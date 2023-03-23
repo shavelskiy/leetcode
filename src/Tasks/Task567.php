2c896c16cad78a600a7c1ddd84d55b9acdb72ae4
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-23 17:49:54 +0300

    task 567 rafactoring

--- src/Tasks/Task567.php
+++ src/Tasks/Task567.php
@@ -6,61 +6,51 @@ namespace App\Tasks;
 
 /**
  * https://leetcode.com/problems/permutation-in-string/
- * 55.17/49.43
+ * 60.92/81.61
  */
 final class Task567
 {
+    private const NEED = 0;
+    private const CURRENT = 1;
+
     public function checkInclusion(string $s1, string $s2): bool
     {
         $n1 = strlen($s1);
+        $iterations = strlen($s2) - $n1;
         $need = $n1;
         $map = [];
         for ($i = 0; $i < $n1; $i++) {
             if (!isset($map[$s1[$i]])) {
                 $map[$s1[$i]] = [
-                    'need' => 0,
-                    'current' => 0,
+                    self::NEED => 0,
+                    self::CURRENT => 0,
                 ];
             }
 
-            $map[$s1[$i]]['need']++;
+            $map[$s1[$i]][self::NEED]++;
         }
 
-        for ($i = 0; $i < $n1; $i++) {
-            if (!isset($map[$s2[$i]])) {
-                continue;
-            }
-
-            if ($map[$s2[$i]]['current'] < $map[$s2[$i]]['need']) {
-                $need--;
-            }
-
-            $map[$s2[$i]]['current']++;
-        }
-
-        if ($need === 0) {
-            return true;
-        }
-
-        for ($i = 1; $i <= strlen($s2) - $n1; $i++) {
-            $prev = $s2[$i - 1];
-            if (isset($map[$prev])) {
-                $map[$prev]['current']--;
-                if ($map[$prev]['current'] < $map[$prev]['need']) {
-                    $need++;
+        for ($i = -1 * $n1; $i < $iterations; $i++) {
+            if ($i >= 0) {
+                $prev = $s2[$i];
+                if (isset($map[$prev])) {
+                    $map[$prev][self::CURRENT]--;
+                    if ($map[$prev][self::CURRENT] < $map[$prev][self::NEED]) {
+                        $need++;
+                    }
                 }
             }
 
-            $next = $s2[$i + $n1 - 1];
+            $next = $s2[$i + $n1];
             if (isset($map[$next])) {
-                if ($map[$next]['current'] < $map[$next]['need']) {
+                if ($map[$next][self::CURRENT] < $map[$next][self::NEED]) {
                     $need--;
                     if ($need === 0) {
                         return true;
                     }
                 }
 
-                $map[$next]['current']++;
+                $map[$next][self::CURRENT]++;
             }
         }
 
