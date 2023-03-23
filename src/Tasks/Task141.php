ee7d5bd133417be53a3b0847c1656a6767ec072a
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-23 15:25:32 +0300

    task 141

---
+++ src/Tasks/Task141.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+use App\Model\ListNode;
+
+/**
+ * https://leetcode.com/problems/linked-list-cycle/.
+ * 69.31/52.57.
+ */
+final class Task141
+{
+    public function hasCycle(?ListNode $head): bool
+    {
+        $fast = $head;
+        while ($head !== null && $fast !== null && $fast->next !== null) {
+            $head = $head->next;
+            $fast = $fast->next->next;
+
+            if ($head === $fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ tests/Task141Test.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Model\ListNode;
+use App\Tasks\Task141;
+use PHPUnit\Framework\TestCase;
+
+final class Task141Test extends TestCase
+{
+    public function test141(): void
+    {
+        $sut = new Task141();
+
+        $data = new ListNode(3, new ListNode(2, new ListNode(0, new ListNode(4))));
+        $copyData = $data;
+        for ($i = 0; $i < 3; $i++) {
+            if ($copyData->next !== null) {
+                $copyData = $copyData->next;
+            }
+        }
+        $copyData->next = $data->next;
+
+        self::assertSame($sut->hasCycle($data), true);
+
+        $data = new ListNode(1, new ListNode(2));
+        $copyData = $data;
+        for ($i = 0; $i < 1; $i++) {
+            if ($copyData->next !== null) {
+                $copyData = $copyData->next;
+            }
+        }
+        $copyData->next = $data->next;
+
+        self::assertSame($sut->hasCycle($data), true);
+
+        $data = new ListNode(1);
+
+        self::assertSame($sut->hasCycle($data), false);
+    }
+}
