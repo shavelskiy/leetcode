c379b284aa551ddc09ef01b39e20ff9b2bb9a7c3
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-24 00:35:44 +0300

    task 2598

---
+++ tests/Task2598Test.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task2598;
+use PHPUnit\Framework\TestCase;
+
+final class Task2598Test extends TestCase
+{
+    public function test2598(): void
+    {
+        $sut = new Task2598();
+
+        self::assertSame($sut->evenOddBit(17), [2, 0]);
+        self::assertSame($sut->evenOddBit(2), [0, 1]);
+    }
+}
