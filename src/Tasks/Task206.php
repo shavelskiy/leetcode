d150c353080e2193e96efe0761f7e942c96ca09a
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-23 11:58:39 +0300

    task 206

---
+++ src/Tasks/Task206.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+use App\Model\ListNode;
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list/.
+ * 69.33/75.56.
+ */
+final class Task206
+{
+    public function reverseList(?ListNode $head): ?ListNode
+    {
+        $last = null;
+        $next = $head;
+        while ($next !== null) {
+            $head = $next;
+            $next = $head->next;
+            $head->next = $last;
+            $last = $head;
+        }
+
+        return $last;
+    }
+}
---
+++ tests/Task206Test.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Model\ListNode;
+use App\Tasks\Task206;
+use PHPUnit\Framework\TestCase;
+
+final class Task206Test extends TestCase
+{
+    public function test206(): void
+    {
+        $sut = new Task206();
+
+        $data = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        self::assertSame($sut->reverseList($data)?->toArray(), [5, 4, 3, 2, 1]);
+
+        $data = new ListNode(1, new ListNode(2));
+        self::assertSame($sut->reverseList($data)?->toArray(), [2, 1]);
+    }
+}
