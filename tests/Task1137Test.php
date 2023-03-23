cdcde29d3695d3edcb2e5217e9dc803bb0793311
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-23 18:51:08 +0300

    task 1137

---
+++ tests/Task1137Test.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task1137;
+use PHPUnit\Framework\TestCase;
+
+final class Task1137Test extends TestCase
+{
+    public function test1137(): void
+    {
+        $sut = new Task1137();
+
+        self::assertSame($sut->tribonacci(0), 0);
+        self::assertSame($sut->tribonacci(4), 4);
+        self::assertSame($sut->tribonacci(25), 1389537);
+    }
+}
