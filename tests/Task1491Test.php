a429dcd585a952db6ec76a8435d7d94f045ed978
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-23 19:06:54 +0300

    1491

---
+++ tests/Task1491Test.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task1491;
+use PHPUnit\Framework\TestCase;
+
+final class Task1491Test extends TestCase
+{
+    public function test1491(): void
+    {
+        $sut = new Task1491();
+
+        self::assertSame($sut->average([4000, 3000, 1000, 2000]), 2500.0);
+        self::assertSame($sut->average([1000, 2000, 3000]), 2000.0);
+    }
+}
