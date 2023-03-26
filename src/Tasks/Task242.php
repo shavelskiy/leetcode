480a9d9604d2ef117d544f26663d4b62f6c4fc21
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-26 18:17:38 +0300

    task 242

---
+++ src/Tasks/Task242.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/valid-anagram/
+ * 78.42/97.89
+ */
+final class Task242
+{
+    public function isAnagram(string $s, string $t): bool
+    {
+        $n = strlen($s);
+
+        if ($n !== strlen($t)) {
+            return false;
+        }
+
+        $map = [];
+        for ($i = 0; $i < $n; $i++) {
+            $map[$s[$i]] = ($map[$s[$i]] ?? 0) + 1;
+            $map[$t[$i]] = ($map[$t[$i]] ?? 0) - 1;
+        }
+
+        foreach ($map as $result) {
+            if ($result !== 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ tests/Task242Test.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task242;
+use PHPUnit\Framework\TestCase;
+
+final class Task242Test extends TestCase
+{
+    public function test242(): void
+    {
+        $sut = new Task242();
+
+        self::assertSame($sut->isAnagram('anagram', 'nagaram'), true);
+        self::assertSame($sut->isAnagram('rat', 'cat'), false);
+    }
+}
