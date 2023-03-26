4bd2d2444697932e816eea4d4c80bc3093fdf092
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-26 18:36:27 +0300

    task 409

---
+++ tests/Task409Test.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task409;
+use PHPUnit\Framework\TestCase;
+
+final class Task409Test extends TestCase
+{
+    public function test409(): void
+    {
+        $sut = new Task409();
+
+        self::assertSame($sut->longestPalindrome('abccccdd'), 7);
+        self::assertSame($sut->longestPalindrome('a'), 1);
+    }
+}
