f2807662822b1a8992d4a99d67554370b8bbfd6c
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-26 20:55:38 +0300

    task 1822

---
+++ tests/Task1822Test.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task1822;
+use PHPUnit\Framework\TestCase;
+
+final class Task1822Test extends TestCase
+{
+    public function test1822(): void
+    {
+        $sut = new Task1822();
+
+        self::assertSame($sut->arraySign([-1, -2, -3, -4, 3, 2, 1]), 1);
+        self::assertSame($sut->arraySign([1, 5, 0, 2, -3]), 0);
+        self::assertSame($sut->arraySign([-1, 1, -1, 1, -1]), -1);
+    }
+}
