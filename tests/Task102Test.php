7dac4b515c0909631b59017e81174496c32a6a36
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-26 20:05:30 +0300

    task 102

---
+++ tests/Task102Test.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Model\TreeNode;
+use App\Tasks\Task102;
+use PHPUnit\Framework\TestCase;
+
+final class Task102Test extends TestCase
+{
+    public function test102(): void
+    {
+        $sut = new Task102();
+
+        $data = new TreeNode(
+            3,
+            new TreeNode(9),
+            new TreeNode(20, new TreeNode(15), new TreeNode(7))
+        );
+
+        self::assertSame($sut->levelOrder($data), [[3], [9, 20], [15, 7]]);
+    }
+}
