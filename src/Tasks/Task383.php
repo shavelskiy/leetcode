a1e6dd3ec691844db90992b70367ae8aa4e822a4
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-26 18:11:44 +0300

    task 383

---
+++ src/Tasks/Task383.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/ransom-note/description/
+ * 92.36/79.17
+ */
+final class Task383
+{
+    public function canConstruct(string $ransomNote, string $magazine): bool
+    {
+        $n = strlen($magazine);
+        $map = [];
+        for ($i = 0; $i < $n; $i++) {
+            $map[$magazine[$i]] = ($map[$magazine[$i]] ?? 0) + 1;
+        }
+
+        $n = strlen($ransomNote);
+        for ($i = 0; $i < $n; $i++) {
+            if (($map[$ransomNote[$i]] ?? 0) < 1) {
+                return false;
+            }
+
+            $map[$ransomNote[$i]]--;
+        }
+
+        return true;
+    }
+}
---
+++ tests/Task383Test.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task383;
+use PHPUnit\Framework\TestCase;
+
+final class Task383Test extends TestCase
+{
+    public function test383(): void
+    {
+        $sut = new Task383();
+
+        self::assertSame($sut->canConstruct('a', 'b'), false);
+        self::assertSame($sut->canConstruct('aa', 'ab'), false);
+        self::assertSame($sut->canConstruct('aa', 'aab'), true);
+    }
+}
