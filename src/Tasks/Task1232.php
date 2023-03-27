ed49fcbd42ec45dde4301fa854fd7193960247d8
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-27 14:34:30 +0300

    task 1232

---
+++ src/Tasks/Task1232.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/check-if-it-is-a-straight-line/
+ * 90.91/81.82
+ */
+final class Task1232
+{
+    /**
+     * @param array<int[]> $coordinates
+     */
+    public function checkStraightLine(array $coordinates): bool
+    {
+        $n = count($coordinates);
+        if ($n <= 2) {
+            return true;
+        }
+
+        $diff = [$coordinates[1][0] - $coordinates[0][0], $coordinates[1][1] - $coordinates[0][1]];
+        for ($i = 2; $i < $n; $i++) {
+            if (
+                $diff[0] * ($coordinates[$i][1] - $coordinates[0][1]) !==
+                $diff[1] * ($coordinates[$i][0] - $coordinates[0][0])
+            ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
