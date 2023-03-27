21ac6cf0cee2f92ef6d811cc6fcb2723102e2b37
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-27 17:57:12 +0300

    task 55

---
+++ src/Tasks/Task55.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/jump-game/
+ * 71.43/45.71
+ */
+final class Task55
+{
+    /**
+     * @param int[] $nums
+     */
+    public function canJump(array $nums): bool
+    {
+        $i = count($nums);
+        $need = 0;
+        while ($i > 1) {
+            $i--;
+            if ($nums[$i - 1] === 0) {
+                $need++;
+                continue;
+            }
+
+            if ($nums[$i - 1] <= $need) {
+                $need++;
+            } else {
+                $need = 0;
+            }
+        }
+
+        return $need === 0;
+    }
+}
