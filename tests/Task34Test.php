46c64f1ef67d3ea5bdad67cfe659988adab45252
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-27 20:45:53 +0300

    task 34

---
+++ tests/Task34Test.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task34;
+use PHPUnit\Framework\TestCase;
+
+final class Task34Test extends TestCase
+{
+    public function test34(): void
+    {
+        $sut = new Task34();
+
+        self::assertSame($sut->searchRange([5, 7, 7, 8, 8, 10], 8), [3, 4]);
+        self::assertSame($sut->searchRange([5, 7, 7, 8, 8, 10], 6), [-1, -1]);
+        self::assertSame($sut->searchRange([], 0), [-1, -1]);
+    }
+}
