cbb0e98509bfee098e76b879f2f1f38ec03dc84f
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-27 18:47:20 +0300

    task 852

---
+++ tests/Task852Test.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task852;
+use PHPUnit\Framework\TestCase;
+
+final class Task852Test extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test852(array $nums, int $result): void
+    {
+        $sut = new Task852();
+
+        self::assertSame($sut->peakIndexInMountainArray($nums), $result);
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            [[0, 1, 0], 1],
+            [[0, 2, 1, 0], 1],
+            [[0, 10, 5, 2], 1],
+        ];
+    }
+}
