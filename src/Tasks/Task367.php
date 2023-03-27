c314564b88126be2b611bf5b7c2a1a7bcb5ecc01
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-27 18:27:08 +0300

    task 367

---
+++ src/Tasks/Task367.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/valid-perfect-square/
+ * 70.97/100
+ */
+final class Task367
+{
+    public function isPerfectSquare(int $num): bool
+    {
+        $start = 0;
+        $end = $num;
+
+        while ($start <= $end) {
+            $current = intdiv($start + $end, 2);
+            $square = $current ** 2;
+
+            if ($square > $num) {
+                $end = $current - 1;
+            } elseif ($square < $num) {
+                $start = $current + 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ tests/Task367Test.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task367;
+use PHPUnit\Framework\TestCase;
+
+final class Task367Test extends TestCase
+{
+    public function test367(): void
+    {
+        $sut = new Task367();
+
+        self::assertSame($sut->isPerfectSquare(16), true);
+        self::assertSame($sut->isPerfectSquare(14), false);
+    }
+}
