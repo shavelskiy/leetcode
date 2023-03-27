04d9e1a9e5a884f9c1bcb69ad8f4a72eee3a088e
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-27 14:55:50 +0300

    task 1667

---
+++ sql/Task1667.sql
@@ -0,0 +1,9 @@
+Create table If Not Exists Users (user_id int, name varchar(40));
+Truncate table Users;
+insert into Users (user_id, name) values (1, 'aLice');
+insert into Users (user_id, name) values (2, 'bOB');
+
+select
+  user_id,
+  concat(upper(substring(name, 1, 1)), lower(substring(name, 2))) as name
+from users order by user_id;
