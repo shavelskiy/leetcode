1a6a767ed94052aae1054c8869328be5f6338459
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-28 13:29:29 +0300

    task 1539

---
+++ src/Tasks/Task1539.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/kth-missing-positive-number/
+ * 55/30
+ */
+final class Task1539
+{
+    /**
+     * @param int[] $arr
+     */
+    public function findKthPositive(array $arr, int $k): int
+    {
+        $left = 0;
+        $right = count($arr) - 1;
+
+        while ($left <= $right) {
+            $current = intdiv($right + $left, 2);
+            if ($arr[$current] - ($current + 1) >= $k) {
+                $right = $current - 1;
+            } else {
+                $left = $current + 1;
+            }
+        }
+
+        return $left + $k;
+    }
+}
