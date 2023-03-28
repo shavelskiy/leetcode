4e3f41681b692437a6ba366123c9da28b5eeff1d
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-28 16:23:39 +0300

    task 617

---
+++ tests/Task617Test.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Model\TreeNode;
+use App\Tasks\Task617;
+use PHPUnit\Framework\TestCase;
+
+final class Task617Test extends TestCase
+{
+    public function test617(): void
+    {
+        $sut = new Task617();
+
+        $root1 = new TreeNode(1, new TreeNode(3, new TreeNode(5)), new TreeNode(2));
+        $root2 = new TreeNode(2, new TreeNode(1, null, new TreeNode(4)), new TreeNode(3, null, new TreeNode(7)));
+
+        $result = $sut->mergeTrees($root1, $root2);
+
+        self::assertSame($result?->val, 3);
+    }
+}
