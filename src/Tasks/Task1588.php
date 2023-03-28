08cdaef1cf3abd7fb39224cc0d5594b51a28c232
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-28 15:50:50 +0300

    task 1588

---
+++ src/Tasks/Task1588.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+ * 72/64
+ */
+final class Task1588
+{
+    /**
+     * @param int[] $arr
+     */
+    public function sumOddLengthSubarrays(array $arr): int
+    {
+        $n = count($arr);
+        $result = 0;
+        for ($i = 0; $i < $n; $i++) {
+            $total = ($i + 1) * ($n - $i);
+            $odd = intdiv($total, 2);
+
+            if ($total % 2 === 1) {
+                $odd++;
+            }
+
+            $result += $odd * $arr[$i];
+        }
+
+        return $result;
+    }
+}
---
+++ tests/Task1588Test.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task1588;
+use PHPUnit\Framework\TestCase;
+
+final class Task1588Test extends TestCase
+{
+    public function test1588(): void
+    {
+        $sut = new Task1588();
+
+        self::assertSame($sut->sumOddLengthSubarrays([1, 4, 2, 5, 3]), 58);
+        self::assertSame($sut->sumOddLengthSubarrays([1, 2]), 3);
+    }
+}
