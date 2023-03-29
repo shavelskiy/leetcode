1e220efb7240c657f1dc5f196a8b31e2b80b096c
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-29 12:07:56 +0300

    task 232

---
+++ src/Tasks/Task232.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/implement-queue-using-stacks/
+ * 9.9/40.91
+ */
+final class Task232
+{
+    private $queue = [];
+
+    public function push(int $x): void
+    {
+        $this->queue[] = $x;
+    }
+
+    public function pop(): ?int
+    {
+        return array_shift($this->queue);
+    }
+
+    public function peek(): ?int
+    {
+        return $this->queue[0] ?? null;
+    }
+
+    public function empty(): bool
+    {
+        return empty($this->queue);
+    }
+}
