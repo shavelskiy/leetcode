af20bab9a41f2c4acf537c46466259f14182726b
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-29 15:28:23 +0300

    task 144

---
+++ src/Tasks/Task144.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+use App\Model\TreeNode;
+
+/**
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/
+ * 60.61/39.39
+ */
+final class Task144
+{
+    /**
+     * @return int[]
+     */
+    public function preorderTraversal(?TreeNode $root): array
+    {
+        $result = [];
+        $stack = [];
+        while ($root !== null || !empty($stack)) {
+            $result[] = $root->val;
+
+            if ($root->right !== null) {
+                $stack[] = $root->right;
+            }
+
+            if ($root->left !== null) {
+                $stack[] = $root->left;
+            }
+
+            $root = array_pop($stack);
+        }
+
+        return $result;
+    }
+}
