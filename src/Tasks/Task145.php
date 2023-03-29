0a0f609d40fdcee8e194a0ee0f12ef4e403e0864
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-29 15:39:47 +0300

    task 145

--- src/Tasks/Task145.php
+++ src/Tasks/Task145.php
@@ -7,27 +7,27 @@ namespace App\Tasks;
 use App\Model\TreeNode;
 
 /**
- * https://leetcode.com/problems/binary-tree-preorder-traversal/
- * 60.61/39.39
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/
+ * 72.41/86.21
  */
-final class Task144
+final class Task145
 {
     /**
      * @return int[]
      */
-    public function preorderTraversal(?TreeNode $root): array
+    public function postorderTraversal(?TreeNode $root): array
     {
         $result = [];
         $stack = [];
         while ($root !== null) {
-            $result[] = $root->val;
-
-            if ($root->right !== null) {
-                $stack[] = $root->right;
-            }
+            $result = [$root->val, ...$result];
 
             if ($root->left !== null) {
                 $stack[] = $root->left;
+            }
+
+            if ($root->right !== null) {
+                $stack[] = $root->right;
             }
 
             $root = array_pop($stack);
---
+++ tests/Task145Test.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Model\TreeNode;
+use App\Tasks\Task145;
+use PHPUnit\Framework\TestCase;
+
+final class Task145Test extends TestCase
+{
+    public function test145(): void
+    {
+        $sut = new Task145();
+
+        $data = new TreeNode(1, new TreeNode(4, new TreeNode(2)), new TreeNode(3));
+        self::assertSame($sut->postorderTraversal($data), [2, 4, 3, 1]);
+
+        $data = new TreeNode(1, null, new TreeNode(2, new TreeNode(3)));
+        self::assertSame($sut->postorderTraversal($data), [3, 2, 1]);
+    }
+}
