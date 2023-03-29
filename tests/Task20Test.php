cfb8c3dcd612c1e4ca3e18775ca9ab8cb950857e
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-29 11:59:54 +0300

    task 20

---
+++ tests/Task20Test.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task20;
+use PHPUnit\Framework\TestCase;
+
+final class Task20Test extends TestCase
+{
+    public function test20(): void
+    {
+        $sut = new Task20();
+
+        self::assertSame($sut->isValid('()'), true);
+        self::assertSame($sut->isValid('()[]{}'), true);
+        self::assertSame($sut->isValid('(])'), false);
+    }
+}
