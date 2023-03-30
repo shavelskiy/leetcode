ea83263058ee9f4dbfb96381a2507d5d66f33ab8
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-31 01:36:36 +0300

    task 120

---
+++ src/Tasks/Task120.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/triangle/
+ * 61.45/100
+ */
+final class Task120
+{
+    /**
+     * @param array<int[]> $triangle
+     */
+    public function minimumTotal(array $triangle): int
+    {
+        for ($level = count($triangle) - 2; $level >= 0; $level--) {
+            for ($i = 0; $i <= $level; $i++) {
+                $triangle[$level][$i] += min(
+                    $triangle[$level + 1][$i],
+                    $triangle[$level + 1][$i + 1],
+                );
+            }
+        }
+
+        return $triangle[0][0];
+    }
+}
