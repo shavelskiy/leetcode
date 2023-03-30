733f2305bd540929db8c71dfb7298fc244ff1c27
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-30 13:49:24 +0300

    task 586

--- sql/Task586.sql
+++ sql/Task586.sql
@@ -5,7 +5,8 @@ insert into orders (order_number, customer_number) values (2, 2);
 insert into orders (order_number, customer_number) values (3, 3);
 insert into orders (order_number, customer_number) values (4, 3);
 
-  select count(*) as count, customer_number
-  from orders
-  group by customer_number
-
+select customer_number
+from orders
+group by customer_number
+order by count(*) desc
+limit 1;
