02579eda3a109234fed497073bb1f491f888c7b4
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-30 17:13:53 +0300

    task 633

---
+++ src/Tasks/Task633.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/sum-of-square-numbers/
+ * 100/100
+ */
+final class Task633
+{
+    public function judgeSquareSum(int $c): bool
+    {
+        $start = 0;
+        $end = (int)ceil(sqrt($c));
+
+        while ($start <= $end) {
+            $current = $start ** 2 + $end ** 2;
+
+            if ($current > $c) {
+                $end--;
+            } elseif ($current < $c) {
+                $start++;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ tests/Task633Test.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task633;
+use PHPUnit\Framework\TestCase;
+
+final class Task633Test extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test633(int $c, bool $result): void
+    {
+        $sut = new Task633();
+
+        self::assertSame($sut->judgeSquareSum($c), $result);
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            [5, true],
+            [3, false],
+        ];
+    }
+}
