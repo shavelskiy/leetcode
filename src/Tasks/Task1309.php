4c493a4807ea02a73574bdf17ca7e287dfb69d30
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-31 02:39:37 +0300

    task 1309

---
+++ src/Tasks/Task1309.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping/
+ * 71.43/100
+ */
+final class Task1309
+{
+    public function freqAlphabets(string $s): string
+    {
+        $result = '';
+
+        for ($i = strlen($s) - 1; $i >= 0; $i--) {
+            $char = $s[$i];
+            if ($char === '#') {
+                $result = chr((int)($s[$i - 2] . $s[$i - 1]) + 96) . $result;
+                $i -= 2;
+            } else {
+                $result = chr((int)$char + 96) . $result;
+            }
+        }
+
+        return $result;
+    }
+}
