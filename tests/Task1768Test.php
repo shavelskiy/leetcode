59fc0d075e5611a095cdc0cec214f808517825d1
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-30 12:13:09 +0300

    task 1768

---
+++ tests/Task1768Test.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task1768;
+use PHPUnit\Framework\TestCase;
+
+final class Task1768Test extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test1768(string $word1, string $word2, string $result): void
+    {
+        $sut = new Task1768();
+
+        self::assertSame($sut->mergeAlternately($word1, $word2), $result);
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            ['abc', 'pqr', 'apbqcr'],
+            ['ab', 'pqrs', 'apbqrs'],
+            ['abcd', 'pq', 'apbqcd'],
+        ];
+    }
+}
