ff8bc483783ef8fb61cda1fe43a1001ad797f17c
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-31 02:17:54 +0300

    task 709

---
+++ src/Tasks/Task709.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/to-lower-case/*
+ */
+final class Task709
+{
+    public function toLowerCase(string $s): string
+    {
+        return strtolower($s);
+    }
+}
