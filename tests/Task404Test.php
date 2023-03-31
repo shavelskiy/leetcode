5c603327aace68125bb1534c6a543db181d2b302
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-31 19:25:02 +0300

    task 404

---
+++ tests/Task404Test.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Model\TreeNode;
+use App\Tasks\Task404;
+use PHPUnit\Framework\TestCase;
+
+final class Task404Test extends TestCase
+{
+    public function test404(): void
+    {
+        $sut = new Task404();
+
+        $data = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+
+        self::assertSame($sut->sumOfLeftLeaves($data), 24);
+    }
+}
