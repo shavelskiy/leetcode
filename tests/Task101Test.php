e49209fe9fc6f33eb36db9c99235d3d040a7ea71
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-31 13:13:57 +0300

    task 101

---
+++ tests/Task101Test.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Model\TreeNode;
+use App\Tasks\Task101;
+use PHPUnit\Framework\TestCase;
+
+final class Task101Test extends TestCase
+{
+    public function test101(): void
+    {
+        $sut = new Task101();
+
+        $data = new TreeNode(
+            1,
+            new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+            new TreeNode(2, new TreeNode(4), new TreeNode(3)),
+        );
+
+        self::assertSame($sut->isSymmetric($data), true);
+
+        $data = new TreeNode(
+            1,
+            new TreeNode(2, null, new TreeNode(3)),
+            new TreeNode(2, null, new TreeNode(3)),
+        );
+
+        self::assertSame($sut->isSymmetric($data), false);
+    }
+}
