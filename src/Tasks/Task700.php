955e18321bd70a9e81337f84811b406e67ae42a9
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-31 14:48:43 +0300

    task 700

---
+++ src/Tasks/Task700.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+use App\Model\TreeNode;
+
+/**
+ * https://leetcode.com/problems/search-in-a-binary-search-tree/
+ */
+final class Task700
+{
+    public function searchBST(?TreeNode $root, int $val): ?TreeNode
+    {
+        while ($root !== null) {
+            if ($root->val > $val) {
+                $root = $root->left;
+            } elseif ($root->val < $val) {
+                $root = $root->right;
+            } else {
+                return $root;
+            }
+        }
+
+        return null;
+    }
+}
