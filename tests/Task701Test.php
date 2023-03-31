7a63d69fd82c97e992d0d28f119ebdd6e3e66089
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-31 15:06:22 +0300

    task 701

---
+++ tests/Task701Test.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Model\TreeNode;
+use App\Tasks\Task701;
+use PHPUnit\Framework\TestCase;
+
+final class Task701Test extends TestCase
+{
+    public function test701(): void
+    {
+        $sut = new Task701();
+
+        $data = new TreeNode(4, new TreeNode(2, new TreeNode(1, new TreeNode(3))), new TreeNode(7));
+
+        $result = $sut->insertIntoBST($data, 5);
+
+        self::assertSame($result->right->left->val, 5);
+    }
+}
