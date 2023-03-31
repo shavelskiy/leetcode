fad2005b71e04d18e1a537c28623029f28a58937
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-31 16:29:09 +0300

    task 62

---
+++ src/Tasks/Task62.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/unique-paths/
+ * 91.7/28.57
+ */
+final class Task62
+{
+    public function uniquePaths(int $m, int $n): int
+    {
+        $result = array_fill(0, $m, array_fill(0, $n, 0));
+        $result[0][0] = 1;
+
+        $m--;
+        $n--;
+
+        for ($i = 0; $i <= $m; $i++) {
+            for ($j = 0; $j <= $n; $j++) {
+                if ($i < $m) {
+                    $result[$i + 1][$j] += $result[$i][$j];
+                }
+
+                if ($j < $n) {
+                    $result[$i][$j + 1] += $result[$i][$j];
+                }
+            }
+        }
+
+        return $result[$m][$n];
+    }
+}
---
+++ tests/Task62Test.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task62;
+use PHPUnit\Framework\TestCase;
+
+final class Task62Test extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test62(int $m, int $n, int $result): void
+    {
+        $sut = new Task62();
+
+        self::assertSame($sut->uniquePaths($m, $n), $result);
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            [3, 7, 28],
+            [3, 2, 3],
+        ];
+    }
+}
