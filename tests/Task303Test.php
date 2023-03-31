9d4127754837ed138863864f3b30693e1dc144a9
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-31 19:59:14 +0300

    task 303

---
+++ tests/Task303Test.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task303;
+use PHPUnit\Framework\TestCase;
+
+final class Task303Test extends TestCase
+{
+    public function test303(): void
+    {
+        $sut = new Task303([-2, 0, 3, -5, 2, -1]);
+
+        self::assertSame($sut->sumRange(0, 2), 1);
+        self::assertSame($sut->sumRange(2, 5), -1);
+        self::assertSame($sut->sumRange(0, 5), -3);
+    }
+}
