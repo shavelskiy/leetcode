1b98a94bfaa578489b4a999ae0dba4b068f8fd34
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-31 17:39:35 +0300

    task 299

---
+++ src/Tasks/Task299.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/bulls-and-cows/
+ * 47.37/21.5
+ */
+final class Task299
+{
+    public function getHint(string $secret, string $guess): string
+    {
+        $map = [];
+        $guessMap = [];
+        for ($i = strlen($secret) - 1; $i >= 0; $i--) {
+            if (!isset($map[$secret[$i]])) {
+                $map[$secret[$i]] = [];
+            }
+
+            $map[$secret[$i]][] = $i;
+
+            if (!isset($guessMap[$guess[$i]])) {
+                $guessMap[$guess[$i]] = [];
+            }
+
+            $guessMap[$guess[$i]][] = $i;
+        }
+
+        $a = 0;
+        $b = 0;
+
+        foreach ($guessMap as $num => $value) {
+            if (!isset($map[$num])) {
+                continue;
+            }
+
+            $curA = 0;
+            foreach ($map[$num] as $position) {
+                if (in_array($position, $value, true)) {
+                    $curA++;
+                }
+            }
+
+            $a += $curA;
+            $b += min(count($map[$num]), count($value)) - $curA;
+        }
+
+        return sprintf('%sA%sB', $a, $b);
+    }
+}
---
+++ tests/Task299Test.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task299;
+use PHPUnit\Framework\TestCase;
+
+final class Task299Test extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test299(string $secret, string $guess, string $result): void
+    {
+        $sut = new Task299();
+
+        self::assertSame($sut->getHint($secret, $guess), $result);
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            ['1807', '7810', '1A3B'],
+            ['1123', '0111', '1A1B'],
+        ];
+    }
+}
