5d82c2a372a42ba1bea1ae1506a078eae84d3b3e
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-04-03 19:51:20 +0300

    task 424

---
+++ src/Tasks/Task424.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/longest-repeating-character-replacement/
+ * 34.61/69.23
+ */
+final class Task424
+{
+    public function characterReplacement(string $s, int $k): int
+    {
+        $map = [];
+        $result = 0;
+
+        $n = strlen($s);
+        $left  = 0;
+
+        for ($right = 0; $right < $n; $right++) {
+            $map[$s[$right]] = ($map[$s[$right]] ?? 0) + 1;
+
+            $lenght = $right - $left + 1;
+            if ($lenght - max(array_values($map)) > $k) {
+                $map[$s[$left]]--;
+                $left++;
+            } else {
+                $result = max($result, $lenght);
+            }
+        }
+
+        return $result;
+    }
+}
