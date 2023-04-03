6205a6710be00e96bf330011ae6238a3988aff78
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-04-03 16:58:40 +0300

    task 1346

---
+++ src/Tasks/Task1346.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+/**
+ * https://leetcode.com/problems/check-if-n-and-its-double-exist/
+ * 61.54/12.82
+ */
+final class Task1346
+{
+    /**
+     * @param int[] $arr
+     */
+    public function checkIfExist(array $arr): bool
+    {
+        $arr1 = [];
+        $arr2 = [];
+        foreach ($arr as $num) {
+            if ($num > 0) {
+                $arr1[] = $num;
+            } else {
+                $arr2[] = abs($num);
+            }
+        }
+
+        return $this->process($arr1) || $this->process($arr2);
+    }
+
+    private function process(array $arr): bool
+    {
+        sort($arr);
+
+        for ($i = count($arr) - 1; $i > 0; $i--) {
+            if (($arr[$i] % 2) > 0) {
+                continue;
+            }
+
+            if ($this->search($arr, intdiv($arr[$i], 2), $i - 1)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function search(array $arr, int $target, int $end): bool
+    {
+        $start = 0;
+
+        while ($start <= $end) {
+            $current = intdiv($start + $end, 2);
+
+            if ($arr[$current] > $target) {
+                $end = $current - 1;
+            } elseif ($arr[$current] < $target) {
+                $start = $current + 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ tests/Task1346Test.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task1346;
+use PHPUnit\Framework\TestCase;
+
+final class Task1346Test extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test1346(array $arr, bool $result): void
+    {
+        $sut = new Task1346();
+
+        self::assertSame($sut->checkIfExist($arr), $result);
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            [[-10, 12, -20, -8, 15], true],
+            [[10, 2, 5, 3], true],
+            [[3, 1, 7, 11], false],
+        ];
+    }
+}
