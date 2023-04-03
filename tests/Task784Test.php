2e2bf19677c05c3106778a7b9746691cf8965f96
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-04-03 19:01:13 +0300

    task 794

---
+++ tests/Task784Test.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task784;
+use PHPUnit\Framework\TestCase;
+
+final class Task784Test extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test784(string $s, array $result): void
+    {
+        $sut = new Task784();
+
+        // dd($sut->letterCasePermutation($s));
+
+        self::assertSame($sut->letterCasePermutation($s), $result);
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            ['C', ['C', 'c']],
+            // ['a1b2', ['a1b2', 'a1B2', 'A1b2', 'A1B2']],
+            // ['3z4', ['3z4', '3Z4']],
+        ];
+    }
+}
