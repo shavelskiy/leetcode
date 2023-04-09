fb363f7af1e0d2d81d2c40797ce30cdc21f8b97b
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-04-09 18:45:41 +0300

    task 1014

---
+++ tests/Task1014Test.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task1014;
+use PHPUnit\Framework\TestCase;
+
+final class Task1014Test extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test1014(array $values, int $result): void
+    {
+        $sut = new Task1014();
+
+        self::assertSame($sut->maxScoreSightseeingPair($values), $result);
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            [[8, 1, 5, 2, 6], 11],
+            [[1, 2], 2],
+        ];
+    }
+}
