c90b2eefcecc84d6525d25d7288cd3d304f79163
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-08-29 21:32:34 +0300

    task 67

---
+++ tests/Task67Test.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task67;
+use PHPUnit\Framework\TestCase;
+
+final class Task67Test extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test67(string $a, string $b, string $result): void
+    {
+        $sut = new Task67();
+
+        self::assertSame($sut->addBinary($a, $b), $result);
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            ['11', '1', '100'],
+            // ['1010', '1011', '10101'],
+        ];
+    }
+}
