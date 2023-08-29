0c46eeb557036b6fcc05ed133fd5630d6034d1fb
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-08-29 21:09:04 +0300

    task 100

---
+++ src/Tasks/Task100.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+use App\Model\TreeNode;
+
+/**
+ * https://leetcode.com/problems/same-tree
+ * 91.00/76.00
+ */
+final class Task100
+{
+    public function isSameTree(?TreeNode $p, ?TreeNode $q): bool
+    {
+        if ($p === null && $q === null) {
+            return true;
+        }
+
+        if ($p?->val !== $q?->val) {
+            return false;
+        }
+
+        return $this->isSameTree($p->left, $q->left) && $this->isSameTree($p->right, $q->right);
+    }
+}
