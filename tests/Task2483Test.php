d156fa1468fc7e517404de3cf2feabeb66e71f10
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-08-29 20:49:08 +0300

    task 2483

---
+++ tests/Task2483Test.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task2483;
+use PHPUnit\Framework\TestCase;
+
+final class Task2483Test extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test2483(string $customers, int $result): void
+    {
+        $sut = new Task2483();
+
+        self::assertSame($sut->bestClosingTime($customers), $result);
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            ['YN', 1],
+            ['YYNY', 2],
+            ['NNNNN', 0],
+            ['YYYY', 4],
+        ];
+    }
+}
