7a1bfc916487651271f3968ce4fd634d5eb1f40c
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-05 13:49:08 +0300

    task 43

---
+++ tests/Task43Test.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task43;
+use PHPUnit\Framework\TestCase;
+
+final class Task43Test extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test54(string $num1, string $num2, string $result): void
+    {
+        $sut = new Task43();
+
+        self::assertSame($sut->multiply($num1, $num2), $result);
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            ['9133', '0', '0'],
+            ['9', '9', '81'],
+            ['2', '3', '6'],
+            ['123', '456', '56088'],
+        ];
+    }
+}
