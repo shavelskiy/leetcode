67b180f16e45fbeb133d1b0c525b4820b6f4fa92
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-05 12:51:26 +0300

    task 860

---
+++ tests/Task860Test.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tasks\Task860;
+use PHPUnit\Framework\TestCase;
+
+final class Task860Test extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     *
+     * @param int[] $bills
+     */
+    public function test54(array $bills, bool $result): void
+    {
+        $sut = new Task860();
+
+        self::assertSame($sut->lemonadeChange($bills), $result);
+    }
+
+    public static function dataProvider(): array
+    {
+        return [
+            [[5, 5, 5, 10, 20], true],
+            [[5, 5, 10, 10, 20], false],
+        ];
+    }
+}
