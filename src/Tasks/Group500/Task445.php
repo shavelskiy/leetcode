ea91630bc41dd2b5ead08ad870c6653403944949
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-06 20:38:04 +0300

    task445

--- src/Tasks/Group500/Task445.php
+++ src/Tasks/Group500/Task445.php
@@ -4,9 +4,43 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group500;
 
+use App\Model\ListNode;
+
+/**
+ * https://leetcode.com/problems/add-two-numbers-ii/
+ * 100.00/60.00
+ */
 final class Task445
 {
-    public function execute(): void
+    public function addTwoNumbers(ListNode $l1, ListNode $l2): ListNode
     {
+        $stack1 = [];
+        $stack2 = [];
+
+        while ($l1 !== null || $l2 !== null) {
+            if ($l1 !== null) {
+                $stack1[] = $l1->val;
+                $l1 = $l1->next;
+            }
+
+            if ($l2 !== null) {
+                $stack2[] = $l2->val;
+                $l2 = $l2->next;
+            }
+        }
+
+        $result = null;
+        $transfer = false;
+        while (!empty($stack1) || !empty($stack2)) {
+            $v = (array_pop($stack1) ?? 0) + (array_pop($stack2) ?? 0) + ($transfer ? 1 : 0);
+            $transfer = $v > 9;
+            $result = new ListNode($v % 10, $result);
+        }
+
+        if ($transfer) {
+            $result = new ListNode(1, $result);
+        }
+
+        return $result;
     }
 }
--- tests/Group500/Task445Test.php
+++ tests/Group500/Task445Test.php
@@ -4,24 +4,40 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group500;
 
+use App\Model\ListNode;
 use PHPUnit\Framework\TestCase;
 
 final class Task445Test extends TestCase
 {
-    /**
-     * @dataProvider dataProvider
-     */
-    public function test445(int $a, int $result): void
+    public function test445(): void
     {
-        $sut = new Task445();
+        $l1 = new ListNode(
+            7,
+            new ListNode(
+                2,
+                new ListNode(
+                    4,
+                    new ListNode(3),
+                ),
+            ),
+        );
 
-        self::assertSame($a, $result);
-    }
+        $l2 = new ListNode(
+            5,
+            new ListNode(
+                6,
+                new ListNode(4),
+            ),
+        );
 
-    public static function dataProvider(): array
-    {
-        return [
-            [1, 1],
-        ];
+        $temp = (new Task445())->addTwoNumbers($l1, $l2);
+
+        $result = [];
+        while ($temp !== null) {
+            $result[] = $temp->val;
+            $temp = $temp->next;
+        }
+
+        self::assertSame($result, [7, 8, 0, 7]);
     }
 }
