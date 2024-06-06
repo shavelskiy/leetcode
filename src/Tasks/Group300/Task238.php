fbce6e442867c1803793aa632243a7d70c547cdb
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-07 01:13:55 +0300

    task 238

--- src/Tasks/Group300/Task238.php
+++ src/Tasks/Group300/Task238.php
@@ -4,9 +4,35 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group300;
 
+/**
+ * https://leetcode.com/problems/product-of-array-except-self/.
+ * 91.49/89.97.
+ */
 final class Task238
 {
-    public function execute(): void
+    /**
+     * @param int[] $nums
+     *
+     * @return int[]
+     */
+    public function productExceptSelf(array $nums): array
     {
+        $n = count($nums);
+        $suffix = [];
+
+        for ($i = 0; $i < $n; ++$i) {
+            $suffix[$i] = ($suffix[$i - 1] ?? 1) * ($nums[$i - 1] ?? 1);
+        }
+
+        $result = array_fill(0, $n, null);
+
+        $postfix = 1;
+        for ($i = 0; $i < $n; ++$i) {
+            $index = $n - $i - 1;
+            $postfix = $postfix * ($nums[$n - $i] ?? 1);
+            $result[$index] = $suffix[$index] * $postfix;
+        }
+
+        return $result;
     }
 }
--- tests/Group300/Task238Test.php
+++ tests/Group300/Task238Test.php
@@ -10,18 +10,22 @@ final class Task238Test extends TestCase
 {
     /**
      * @dataProvider dataProvider
+     *
+     * @param int[] $nums
+     * @param int[] $result
      */
-    public function test238(int $a, int $result): void
+    public function test238(array $nums, array $result): void
     {
         $sut = new Task238();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->productExceptSelf($nums, $result), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[1, 2, 3, 4], [24, 12, 8, 6]],
+            [[-1, 1, 0, -3, 3], [0, 0, 9, 0, 0]],
         ];
     }
 }
