fd74eb728c44ecd3893a0e6c4f6e4c6805d1f71d
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-07 19:06:43 +0300

    task 1679

--- src/Tasks/Group1700/Task1679.php
+++ src/Tasks/Group1700/Task1679.php
@@ -4,9 +4,38 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group1700;
 
+/**
+ * https://leetcode.com/problems/max-number-of-k-sum-pairs/.
+ * 52.78/25.00.
+ */
 final class Task1679
 {
-    public function execute(): void
+    /**
+     * @param int[] $nums
+     */
+    public function maxOperations(array $nums, int $k): int
     {
+        $result = 0;
+
+        sort($nums);
+
+        $left = 0;
+        $right = count($nums) - 1;
+
+        while ($left < $right) {
+            $sum = $nums[$left] + $nums[$right];
+
+            if ($sum < $k) {
+                ++$left;
+            } elseif ($sum > $k) {
+                --$right;
+            } else {
+                ++$result;
+                ++$left;
+                --$right;
+            }
+        }
+
+        return $result;
     }
 }
--- tests/Group1700/Task1679Test.php
+++ tests/Group1700/Task1679Test.php
@@ -10,18 +10,21 @@ final class Task1679Test extends TestCase
 {
     /**
      * @dataProvider dataProvider
+     *
+     * @param int[] $nums
      */
-    public function test1679(int $a, int $result): void
+    public function test1679(array $nums, int $k, int $result): void
     {
         $sut = new Task1679();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->maxOperations($nums, $k), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[1, 2, 3, 4], 5, 2],
+            [[3, 1, 3, 4, 3], 6, 1],
         ];
     }
 }
