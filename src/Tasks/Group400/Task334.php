62987ffbc9ac1ec4d9a61485462f22b0d27ad719
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-07 12:30:18 +0300

    task334

--- src/Tasks/Group400/Task334.php
+++ src/Tasks/Group400/Task334.php
@@ -4,9 +4,33 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group400;
 
+/**
+ * https://leetcode.com/problems/increasing-triplet-subsequence/.
+ * 36.36/61.82.
+ */
 final class Task334
 {
-    public function execute(): void
+    /**
+     * @param int[] $nums
+     */
+    public function increasingTriplet(array $nums): bool
     {
+        if (count($nums) < 3) {
+            return false;
+        }
+
+        $min1 = null;
+        $min2 = null;
+        for ($i = 0; $i < count($nums); ++$i) {
+            if ($nums[$i] <= $min1 || $min1 === null) {
+                $min1 = $nums[$i];
+            } elseif ($nums[$i] <= $min2 || $min2 === null) {
+                $min2 = $nums[$i];
+            } else {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- tests/Group400/Task334Test.php
+++ tests/Group400/Task334Test.php
@@ -10,18 +10,24 @@ final class Task334Test extends TestCase
 {
     /**
      * @dataProvider dataProvider
+     *
+     * @param int[] $nums
      */
-    public function test334(int $a, int $result): void
+    public function test334(array $nums, bool $result): void
     {
         $sut = new Task334();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->increasingTriplet($nums), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[2, 4, -2, -3], false],
+            [[2, 1, 5], false],
+            [[1, 2, 3, 4, 5], true],
+            [[5, 4, 3, 2, 1], false],
+            [[2, 1, 5, 0, 4, 6], true],
         ];
     }
 }
