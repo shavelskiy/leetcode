448510167915ca20f3def585752eb8ce02060513
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-08 15:47:12 +0300

    task 643

--- src/Tasks/Group700/Task643.php
+++ src/Tasks/Group700/Task643.php
@@ -4,9 +4,48 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group700;
 
+/**
+ * https://leetcode.com/problems/maximum-average-subarray-i.
+ * 20.61/49.09.
+ */
 final class Task643
 {
-    public function execute(): void
+    /**
+     * @param int[] $nums
+     */
+    public function findMaxAverage(array $nums, int $k): float
     {
+        $sum = $nums[0];
+
+        if (count($nums) < 2) {
+            return $sum / $k;
+        }
+
+        $left = 0;
+        $right = 0;
+
+        $max = null;
+
+        while ($right < count($nums) - 1) {
+            if ($right - $left + 1 < $k) {
+                ++$right;
+                $sum += $nums[$right];
+
+                if ($right - $left + 1 === $k) {
+                    $max = $sum;
+                }
+                continue;
+            }
+
+            $sum -= $nums[$left];
+            ++$left;
+            ++$right;
+            $sum += $nums[$right];
+            if ($sum > $max) {
+                $max = $sum;
+            }
+        }
+
+        return ($max ?? $sum) / $k;
     }
 }
--- tests/Group700/Task643Test.php
+++ tests/Group700/Task643Test.php
@@ -10,18 +10,22 @@ final class Task643Test extends TestCase
 {
     /**
      * @dataProvider dataProvider
+     *
+     * @param int[] $nums
      */
-    public function test643(int $a, int $result): void
+    public function test643(array $nums, int $k, float $result): void
     {
         $sut = new Task643();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->findMaxAverage($nums, $k), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[9, 7, 3, 5, 6, 2, 0, 8, 1, 9], 6, 5.333333333333333],
+            [[1, 12, -5, -6, 50, 3], 4, 12.75],
+            [[5], 1, 5.0],
         ];
     }
 }
