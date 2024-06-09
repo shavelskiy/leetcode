595e487a9ff7fb5bfd0af4527d92d2e353bffee4
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-09 16:12:58 +0300

    task 1004

--- src/Tasks/Group1100/Task1004.php
+++ src/Tasks/Group1100/Task1004.php
@@ -4,9 +4,42 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group1100;
 
+/**
+ * https://leetcode.com/problems/max-consecutive-ones-iii/.
+ * 13.51/91.89.
+ */
 final class Task1004
 {
-    public function execute(): void
+    /**
+     * @param int[] $nums
+     */
+    public function longestOnes(array $nums, int $k): int
     {
+        $n = count($nums);
+
+        $left = 0;
+        $max = null;
+
+        for ($right = 0; $right < $n; ++$right) {
+            if ($nums[$right] === 0) {
+                --$k;
+            }
+
+            while ($k < 0 && $left < $n) {
+                if ($nums[$left] === 0) {
+                    ++$k;
+                }
+                ++$left;
+            }
+
+            if ($k >= 0 && $right - $left > $max) {
+                $max = $right - $left;
+                if ($n - $left < $max) {
+                    break;
+                }
+            }
+        }
+
+        return ($max ?? 0) + 1;
     }
 }
--- tests/Group1100/Task1004Test.php
+++ tests/Group1100/Task1004Test.php
@@ -10,18 +10,25 @@ final class Task1004Test extends TestCase
 {
     /**
      * @dataProvider dataProvider
+     *
+     * @param int[] $nums
      */
-    public function test1004(int $a, int $result): void
+    public function test1004(array $nums, int $k, int $result): void
     {
         $sut = new Task1004();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->longestOnes($nums, $k), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[0], 1, 1],
+            [[1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1], 8, 25],
+            [[0, 0, 0, 0], 0, 0],
+            [[0, 0, 1, 1, 1, 0, 0], 0, 3],
+            [[1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2, 6],
+            [[0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3, 10],
         ];
     }
 }
