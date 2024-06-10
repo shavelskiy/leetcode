181734da2635068b611e167d70f98c00538925e0
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-11 00:25:17 +0300

    task 328

--- src/Tasks/Group400/Task328.php
+++ src/Tasks/Group400/Task328.php
@@ -4,9 +4,42 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group400;
 
+use App\Model\ListNode;
+
+/**
+ * https://leetcode.com/problems/odd-even-linked-list/.
+ * 51.35/37.84.
+ */
 final class Task328
 {
-    public function execute(): void
+    public function oddEvenList(?ListNode $head): ?ListNode
     {
+        if ($head === null || $head->next === null) {
+            return $head;
+        }
+
+        $odd = $head;
+        $even = $head->next;
+        $evenHead = $even;
+
+        $current = $head->next->next;
+        $isOdd = true;
+        while ($current !== null) {
+            if ($isOdd) {
+                $odd->next = $current;
+                $odd = $odd->next;
+            } else {
+                $even->next = $current;
+                $even = $even->next;
+            }
+
+            $current = $current->next;
+            $isOdd = !$isOdd;
+        }
+
+        $odd->next = $evenHead;
+        $even->next = null;
+
+        return $head;
     }
 }
--- tests/Group400/Task328Test.php
+++ tests/Group400/Task328Test.php
@@ -4,24 +4,17 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group400;
 
+use App\Model\ListNode;
 use PHPUnit\Framework\TestCase;
 
 final class Task328Test extends TestCase
 {
-    /**
-     * @dataProvider dataProvider
-     */
-    public function test328(int $a, int $result): void
+    public function test328(): void
     {
         $sut = new Task328();
 
-        self::assertSame($a, $result);
-    }
+        $list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
 
-    public static function dataProvider(): array
-    {
-        return [
-            [1, 1],
-        ];
+        self::assertSame($sut->oddEvenList($list)->toArray(), [1, 3, 5, 2, 4]);
     }
 }
