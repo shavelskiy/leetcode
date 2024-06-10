a8ef935be092c80052e5a02f56834e1074586a58
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-10 11:22:58 +0300

    task 1493

--- src/Tasks/Group1500/Task1493.php
+++ src/Tasks/Group1500/Task1493.php
@@ -4,9 +4,40 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group1500;
 
+/**
+ * https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/.
+ * 81.25/87.50.
+ */
 final class Task1493
 {
-    public function execute(): void
+    /**
+     * @param int[] $nums
+     */
+    public function longestSubarray(array $nums): int
     {
+        $n = count($nums);
+
+        $left = 0;
+        $max = 0;
+        $countZero = 0;
+
+        for ($right = 0; $right < $n; ++$right) {
+            if ($nums[$right] === 0) {
+                ++$countZero;
+            }
+
+            while ($countZero > 1) {
+                if ($nums[$left] === 0) {
+                    --$countZero;
+                }
+                ++$left;
+            }
+
+            if ($right - $left > $max) {
+                $max = $right - $left;
+            }
+        }
+
+        return $max;
     }
 }
--- tests/Group1500/Task1493Test.php
+++ tests/Group1500/Task1493Test.php
@@ -10,18 +10,24 @@ final class Task1493Test extends TestCase
 {
     /**
      * @dataProvider dataProvider
+     *
+     * @param int[] $nums
      */
-    public function test1493(int $a, int $result): void
+    public function test1493(array $nums, int $result): void
     {
         $sut = new Task1493();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->longestSubarray($nums), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[0, 0, 0], 0],
+            [[1, 1, 0, 0, 1, 1, 1, 0, 1], 4],
+            [[1, 1, 0, 1], 3],
+            [[0, 1, 1, 1, 0, 1, 1, 0, 1], 5],
+            [[1, 1, 1], 2],
         ];
     }
 }
