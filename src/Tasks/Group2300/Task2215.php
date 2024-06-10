74e0c679409b42212cb8b9f2770371780d9626b0
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-10 11:37:47 +0300

    task2215

--- src/Tasks/Group2300/Task2215.php
+++ src/Tasks/Group2300/Task2215.php
@@ -4,9 +4,46 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group2300;
 
+/**
+ * https://leetcode.com/problems/find-the-difference-of-two-arrays/.
+ */
 final class Task2215
 {
-    public function execute(): void
+    /**
+     * @param int[] $nums1
+     * @param int[] $nums2
+     *
+     * @return array<int[]>
+     */
+    public function findDifference(array $nums1, array $nums2): array
     {
+        return [
+            $this->getResult($nums1, $this->getHashMap($nums2)),
+            $this->getResult($nums2, $this->getHashMap($nums1)),
+        ];
+    }
+
+    private function getHashMap(array $nums): array
+    {
+        $result = [];
+
+        foreach ($nums as $num) {
+            $result[$num] = true;
+        }
+
+        return $result;
+    }
+
+    private function getResult(array $nums, array $hash): array
+    {
+        $result = [];
+
+        foreach ($nums as $num) {
+            if (!isset($hash[$num])) {
+                $result[$num] = true;
+            }
+        }
+
+        return array_keys($result);
     }
 }
--- tests/Group2300/Task2215Test.php
+++ tests/Group2300/Task2215Test.php
@@ -10,18 +10,23 @@ final class Task2215Test extends TestCase
 {
     /**
      * @dataProvider dataProvider
+     *
+     * @param int[]        $nums1
+     * @param int[]        $nums2
+     * @param array<int[]> $result
      */
-    public function test2215(int $a, int $result): void
+    public function test2215(array $nums1, array $nums2, array $result): void
     {
         $sut = new Task2215();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->findDifference($nums1, $nums2), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[1, 2, 3], [2, 4, 6], [[1, 3], [4, 6]]],
+            [[1, 2, 3, 3], [1, 1, 2, 2], [[3], []]],
         ];
     }
 }
