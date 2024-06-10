e310dcaa6002f3fb64fe1fa82263ab3bc53b13c7
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-10 22:24:48 +0300

    task 735

--- src/Tasks/Group800/Task735.php
+++ src/Tasks/Group800/Task735.php
@@ -4,9 +4,52 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group800;
 
+/**
+ * https://leetcode.com/problems/asteroid-collision/.
+ * 70.00/100.00.
+ */
 final class Task735
 {
-    public function execute(): void
+    /**
+     * @param int[] $asteroids
+     *
+     * @return int[]
+     */
+    public function asteroidCollision(array $asteroids): array
     {
+        $stack = [];
+
+        foreach ($asteroids as $asteroid) {
+            $this->process($stack, $asteroid);
+        }
+
+        return $stack;
+    }
+
+    private function process(array &$stack, int $asteroid): void
+    {
+        while (true) {
+            if (empty($stack)) {
+                $stack[] = $asteroid;
+                return;
+            }
+
+            $temp = array_pop($stack);
+
+            if ($asteroid > 0 || $temp < 0) {
+                $stack[] = $temp;
+                $stack[] = $asteroid;
+                return;
+            }
+
+            if ($temp > abs($asteroid)) {
+                $stack[] = $temp;
+                return;
+            }
+
+            if ($temp === abs($asteroid)) {
+                return;
+            }
+        }
     }
 }
--- tests/Group800/Task735Test.php
+++ tests/Group800/Task735Test.php
@@ -10,18 +10,26 @@ final class Task735Test extends TestCase
 {
     /**
      * @dataProvider dataProvider
+     *
+     * @param int[] $asteroids
+     *
+     * @return int[]
      */
-    public function test735(int $a, int $result): void
+    public function test735(array $asteroids, array $result): void
     {
         $sut = new Task735();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->asteroidCollision($asteroids), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[-2, -2, 1, -2], [-2, -2, -2]],
+            [[-2, -1, 1, 2], [-2, -1, 1, 2]],
+            [[5, 10, -5], [5, 10]],
+            [[8, -8], []],
+            [[10, 2, -5], [10]],
         ];
     }
 }
