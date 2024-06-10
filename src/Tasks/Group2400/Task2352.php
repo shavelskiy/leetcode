facd4ca76b83e31c286a5005d1468a8cbb2cc5f7
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-10 17:03:24 +0300

    task 2352

--- src/Tasks/Group2400/Task2352.php
+++ src/Tasks/Group2400/Task2352.php
@@ -4,9 +4,49 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group2400;
 
+/**
+ * https://leetcode.com/problems/equal-row-and-column-pairs/.
+ * 61.11/11.11.
+ */
 final class Task2352
 {
-    public function execute(): void
+    /**
+     * @param array<int[]> $grid
+     */
+    public function equalPairs(array $grid): int
     {
+        $n = count($grid);
+
+        $hash = [];
+
+        for ($i = 0; $i < $n; ++$i) {
+            $row = '';
+            $column = '';
+            for ($j = 0; $j < $n; ++$j) {
+                $row .= '.' . $grid[$i][$j];
+                $column .= '.' . $grid[$j][$i];
+            }
+
+            if (!isset($hash[$row][0])) {
+                $hash[$row][0] = 0;
+            }
+
+            if (!isset($hash[$column][1])) {
+                $hash[$column][1] = 0;
+            }
+
+            ++$hash[$row][0];
+            ++$hash[$column][1];
+        }
+
+        $result = 0;
+
+        foreach ($hash as $values) {
+            if (count($values) > 1) {
+                $result += $values[0] * $values[1];
+            }
+        }
+
+        return $result;
     }
 }
--- tests/Group2400/Task2352Test.php
+++ tests/Group2400/Task2352Test.php
@@ -10,18 +10,22 @@ final class Task2352Test extends TestCase
 {
     /**
      * @dataProvider dataProvider
+     *
+     * @param array<int[]> $grid
      */
-    public function test2352(int $a, int $result): void
+    public function test2352(array $grid, int $result): void
     {
         $sut = new Task2352();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->equalPairs($grid), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[[11, 1], [1, 11]], 2],
+            [[[3, 2, 1], [1, 7, 6], [2, 7, 7]], 1],
+            [[[3, 1, 2, 2], [1, 4, 4, 5], [2, 4, 2, 2], [2, 4, 2, 2]], 3],
         ];
     }
 }
