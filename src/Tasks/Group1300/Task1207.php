da81b44f1c186ea9e565eca95d0860402032469a
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-10 11:43:40 +0300

    task 1207

--- src/Tasks/Group1300/Task1207.php
+++ src/Tasks/Group1300/Task1207.php
@@ -4,9 +4,36 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group1300;
 
+/**
+ * https://leetcode.com/problems/unique-number-of-occurrences/.
+ * 100.00/58.70.
+ */
 final class Task1207
 {
-    public function execute(): void
+    /**
+     * @param int[] $arr
+     */
+    public function uniqueOccurrences(array $arr): bool
     {
+        $hash = [];
+
+        foreach ($arr as $num) {
+            if (!isset($hash[$num])) {
+                $hash[$num] = 0;
+            }
+
+            ++$hash[$num];
+        }
+
+        $resultHash = [];
+        foreach ($hash as $count) {
+            if (isset($resultHash[$count])) {
+                return false;
+            }
+
+            $resultHash[$count] = true;
+        }
+
+        return true;
     }
 }
--- tests/Group1300/Task1207Test.php
+++ tests/Group1300/Task1207Test.php
@@ -10,18 +10,23 @@ final class Task1207Test extends TestCase
 {
     /**
      * @dataProvider dataProvider
+     *
+     * @param int[] $arr
      */
-    public function test1207(int $a, int $result): void
+    public function test1207(array $arr, bool $result): void
     {
         $sut = new Task1207();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->uniqueOccurrences($arr), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[1, 2, 2, 1, 1, 3], true],
+            [[1, 2], false],
+            [[-3, 0, 1, -3, 1, 1, 1, -3, 10, 0], true],
+            [[3, 5, -2, -3, -6, -6], false],
         ];
     }
 }
