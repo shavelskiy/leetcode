104d90f023dcb3068891dbdd668feb9e4e2780dc
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-10 11:28:17 +0300

    task 1732

--- src/Tasks/Group1800/Task1732.php
+++ src/Tasks/Group1800/Task1732.php
@@ -4,9 +4,25 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group1800;
 
+/**
+ * https://leetcode.com/problems/find-the-highest-altitude/.
+ * 26.23/86.89.
+ */
 final class Task1732
 {
-    public function execute(): void
+    /**
+     * @param int[] $gain
+     */
+    public function largestAltitude(array $gain): int
     {
+        $max = 0;
+        $current = 0;
+
+        foreach ($gain as $item) {
+            $current += $item;
+            $max = max($max, $current);
+        }
+
+        return $max;
     }
 }
--- tests/Group1800/Task1732Test.php
+++ tests/Group1800/Task1732Test.php
@@ -10,18 +10,21 @@ final class Task1732Test extends TestCase
 {
     /**
      * @dataProvider dataProvider
+     *
+     * @param int[] $gain
      */
-    public function test1732(int $a, int $result): void
+    public function test1732(array $gain, int $result): void
     {
         $sut = new Task1732();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->largestAltitude($gain), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[-5, 1, 5, 0, -7], 1],
+            [[-4, -3, -2, -1, 4, 3, 2], 0],
         ];
     }
 }
