807f9b7fcf0c1b5944452f3b496bd1fe44a79330
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-11 22:45:37 +0300

    task 547

--- src/Tasks/Group600/Task547.php
+++ src/Tasks/Group600/Task547.php
@@ -4,9 +4,50 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group600;
 
+/**
+ * https://leetcode.com/problems/number-of-provinces/.
+ * 14.81/29.26.
+ */
 final class Task547
 {
-    public function execute(): void
+    /**
+     * @param array<int[]> $isConnected
+     */
+    public function findCircleNum($isConnected): int
     {
+        $cities = array_fill(0, count($isConnected), []);
+
+        for ($i = 0; $i < count($isConnected); ++$i) {
+            for ($j = 0; $j < count($isConnected); ++$j) {
+                if ($i !== $j && $isConnected[$i][$j] === 1) {
+                    $cities[$i][] = $j;
+                }
+            }
+        }
+
+        $result = 0;
+        for ($i = 0; $i < count($cities); ++$i) {
+            if ($cities[$i] === null) {
+                continue;
+            }
+
+            $lines = $cities[$i];
+
+            while (!empty($lines)) {
+                $item = array_shift($lines);
+                if ($cities[$item] !== null) {
+                    foreach ($cities[$item] as $line) {
+                        if ($cities[$line] !== null) {
+                            $lines[] = $line;
+                        }
+                    }
+                    $cities[$item] = null;
+                }
+            }
+
+            ++$result;
+        }
+
+        return $result;
     }
 }
--- tests/Group600/Task547Test.php
+++ tests/Group600/Task547Test.php
@@ -10,18 +10,22 @@ final class Task547Test extends TestCase
 {
     /**
      * @dataProvider dataProvider
+     *
+     * @param array<int[]> $isConnected
      */
-    public function test547(int $a, int $result): void
+    public function test547(array $isConnected, int $result): void
     {
         $sut = new Task547();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->findCircleNum($isConnected), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[[1, 0, 0, 1], [0, 1, 1, 0], [0, 1, 1, 1], [1, 0, 1, 1]], 1],
+            [[[1, 1, 0], [1, 1, 0], [0, 0, 1]], 2],
+            [[[1, 0, 0], [0, 1, 0], [0, 0, 1]], 3],
         ];
     }
 }
