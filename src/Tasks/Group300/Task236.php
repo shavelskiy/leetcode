77b710bad8e9b54bef57f9bd00feaa98690e9ad4
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-11 16:34:31 +0300

    task 236

--- src/Tasks/Group300/Task236.php
+++ src/Tasks/Group300/Task236.php
@@ -4,9 +4,51 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group300;
 
+use App\Model\TreeNode;
+
+/**
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/.
+ * 5.56/94.44.
+ */
 final class Task236
 {
-    public function execute(): void
+    public function lowestCommonAncestor(TreeNode $root, TreeNode $p, TreeNode $q): TreeNode
     {
+        if (in_array($root, [$p, $q], true)) {
+            return $root;
+        }
+
+        $left = $this->contains($root->left, $p, $q);
+
+        if ($left === 0) {
+            return $this->lowestCommonAncestor($root->right, $p, $q);
+        }
+
+        if ($left === 1) {
+            return $root;
+        }
+
+        return $this->lowestCommonAncestor($root->left, $p, $q);
+    }
+
+    private function contains(?TreeNode $root, TreeNode $p, TreeNode $q, int $result = 0): int
+    {
+        if ($root === null) {
+            return $result;
+        }
+
+        if ($root === $p) {
+            ++$result;
+        }
+
+        if ($root === $q) {
+            ++$result;
+        }
+
+        if ($result === 2) {
+            return $result;
+        }
+
+        return $result + $this->contains($root->left, $p, $q) + $this->contains($root->right, $p, $q);
     }
 }
--- tests/Group300/Task236Test.php
+++ tests/Group300/Task236Test.php
@@ -4,24 +4,40 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group300;
 
+use App\Model\TreeNode;
 use PHPUnit\Framework\TestCase;
 
 final class Task236Test extends TestCase
 {
-    /**
-     * @dataProvider dataProvider
-     */
-    public function test236(int $a, int $result): void
+    public function test236(): void
     {
         $sut = new Task236();
 
-        self::assertSame($a, $result);
-    }
+        $root = new TreeNode(1, new TreeNode(2));
 
-    public static function dataProvider(): array
-    {
-        return [
-            [1, 1],
-        ];
+        self::assertSame(
+            $sut->lowestCommonAncestor($root, $root, $root->left),
+            $root,
+        );
+
+        $root = new TreeNode(
+            3,
+            new TreeNode(
+                5,
+                new TreeNode(6),
+                new TreeNode(2, new TreeNode(7), new TreeNode(4)),
+            ),
+            new TreeNode(1, new TreeNode(0), new TreeNode(8)),
+        );
+
+        self::assertSame(
+            $sut->lowestCommonAncestor($root, $root->left, $root->left->right->right),
+            $root->left,
+        );
+
+        self::assertSame(
+            $sut->lowestCommonAncestor($root, $root->left, $root->right),
+            $root,
+        );
     }
 }
