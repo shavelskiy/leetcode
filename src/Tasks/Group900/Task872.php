24da3e0ad8e46ef8ac78fffa5993054ad8dd4b16
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-11 13:25:36 +0300

    task 872

--- src/Tasks/Group900/Task872.php
+++ src/Tasks/Group900/Task872.php
@@ -4,9 +4,32 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group900;
 
+use App\Model\TreeNode;
+
+/**
+ * https://leetcode.com/problems/leaf-similar-trees/.
+ * 80.77/61.54.
+ */
 final class Task872
 {
-    public function execute(): void
+    public function leafSimilar(TreeNode $root1, TreeNode $root2): bool
     {
+        return $this->parseTree($root1) === $this->parseTree($root2);
+    }
+
+    private function parseTree(?TreeNode $root): array
+    {
+        if ($root === null) {
+            return [];
+        }
+
+        if ($root->left === null && $root->right === null) {
+            return [$root->val];
+        }
+
+        return [
+            ...$this->parseTree($root->left),
+            ...$this->parseTree($root->right),
+        ];
     }
 }
--- tests/Group900/Task872Test.php
+++ tests/Group900/Task872Test.php
@@ -4,24 +4,46 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group900;
 
+use App\Model\TreeNode;
 use PHPUnit\Framework\TestCase;
 
 final class Task872Test extends TestCase
 {
-    /**
-     * @dataProvider dataProvider
-     */
-    public function test872(int $a, int $result): void
+    public function test872(): void
     {
         $sut = new Task872();
 
-        self::assertSame($a, $result);
-    }
+        $roo1 = new TreeNode(
+            119,
+            new TreeNode(
+                113,
+                new TreeNode(11, new TreeNode(43, new TreeNode(67), new TreeNode(74)), new TreeNode(76)),
+                new TreeNode(30, new TreeNode(15), new TreeNode(60)),
+            ),
+        );
 
-    public static function dataProvider(): array
-    {
-        return [
-            [1, 1],
-        ];
+        $roo2 = new TreeNode(
+            11,
+            new TreeNode(69, new TreeNode(115, new TreeNode(67), new TreeNode(74)), new TreeNode(66, null, new TreeNode(76))),
+            new TreeNode(60, new TreeNode(15), new TreeNode(60)),
+        );
+
+        self::assertSame($sut->leafSimilar($roo1, $roo2), true);
+
+        self::assertSame($sut->leafSimilar(new TreeNode(1), new TreeNode(2)), false);
+
+        $root1 = new TreeNode(
+            3,
+            new TreeNode(5, new TreeNode(6), new TreeNode(2, new TreeNode(7), new TreeNode(4))),
+            new TreeNode(1, new TreeNode(9), new TreeNode(8)),
+        );
+
+        $root2 = new TreeNode(
+            3,
+            new TreeNode(5, new TreeNode(6), new TreeNode(7)),
+            new TreeNode(1, new TreeNode(4), new TreeNode(2, new TreeNode(9), new TreeNode(8))),
+        );
+
+        self::assertSame($sut->leafSimilar($root1, $root2), true);
     }
 }
