67e50266fe738c73de36085ad6459ee485477acd
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-12 14:03:37 +0300

    task 1466

--- src/Tasks/Group1500/Task1466.php
+++ src/Tasks/Group1500/Task1466.php
@@ -4,9 +4,43 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group1500;
 
+/**
+ * https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/.
+ * 88.89/11.11.
+ */
 final class Task1466
 {
-    public function execute(): void
+    private array $in;
+    private array $out;
+    private int $result = 0;
+
+    /**
+     * @param array<int[]> $connections
+     */
+    public function minReorder(int $n, array $connections): int
     {
+        $this->in = array_fill(0, $n, []);
+        $this->out = array_fill(0, $n, []);
+        foreach ($connections as $connection) {
+            $this->in[$connection[1]][$connection[0]] = true;
+            $this->out[$connection[0]][$connection[1]] = true;
+        }
+
+        $this->dfs(0);
+        return $this->result;
+    }
+
+    private function dfs(int $node): void
+    {
+        foreach (array_keys($this->out[$node]) as $out) {
+            ++$this->result;
+            unset($this->in[$out][$node]);
+            $this->dfs($out);
+        }
+
+        foreach (array_keys($this->in[$node]) as $in) {
+            unset($this->out[$in][$node]);
+            $this->dfs($in);
+        }
     }
 }
--- tests/Group1500/Task1466Test.php
+++ tests/Group1500/Task1466Test.php
@@ -10,18 +10,22 @@ final class Task1466Test extends TestCase
 {
     /**
      * @dataProvider dataProvider
+     *
+     * @param array<int[] $connections
      */
-    public function test1466(int $a, int $result): void
+    public function test1466(int $n, array $connections, int $result): void
     {
         $sut = new Task1466();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->minReorder($n, $connections), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [6, [[0, 1], [1, 3], [2, 3], [4, 0], [4, 5]], 3],
+            [5, [[1, 0], [1, 2], [3, 2], [3, 4]], 2],
+            [3, [[1, 0], [2, 0]], 0],
         ];
     }
 }
