3fe685159a76082d75748a6f8322739fb728a722
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-06-12 16:09:12 +0300

    task 399

--- src/Tasks/Group400/Task399.php
+++ src/Tasks/Group400/Task399.php
@@ -4,9 +4,78 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group400;
 
+/**
+ * https://leetcode.com/problems/evaluate-division/.
+ * 100.00/50.00.
+ */
 final class Task399
 {
-    public function execute(): void
+    /**
+     * @param array<string[]> $equations
+     * @param float[]         $values
+     * @param array<string[]> $queries
+     *
+     * @return float[]
+     */
+    public function calcEquation(array $equations, array $values, array $queries): array
     {
+        $map = $this->buildMap($equations, $values);
+
+        $result = [];
+
+        foreach ($queries as $query) {
+            $visited = [];
+            $result[] = $this->dfs($map, $query[0], $query[1], $visited) ?? -1.0;
+        }
+
+        return $result;
+    }
+
+    private function dfs(array $map, string $node, string $to, array &$visited): ?float
+    {
+        if (!isset($map[$node])) {
+            return null;
+        }
+
+        if ($node === $to) {
+            return 1.0;
+        }
+
+        $visited[$node] = true;
+
+        foreach ($map[$node] as $out => $value) {
+            if (isset($visited[$out])) {
+                continue;
+            }
+            $temp = $this->dfs($map, $out, $to, $visited);
+
+            if ($temp !== null) {
+                return $value * $temp;
+            }
+        }
+
+        return null;
+    }
+
+    private function buildMap(array $equations, array $values): array
+    {
+        $map = [];
+
+        foreach ($equations as $i => $equation) {
+            [$from, $to] = $equation;
+
+            if (!isset($map[$from])) {
+                $map[$from] = [];
+            }
+
+            if (!isset($map[$to])) {
+                $map[$to] = [];
+            }
+
+            $map[$from][$to] = $values[$i];
+            $map[$to][$from] = 1 / $values[$i];
+        }
+
+        return $map;
     }
 }
--- tests/Group400/Task399Test.php
+++ tests/Group400/Task399Test.php
@@ -10,18 +10,27 @@ final class Task399Test extends TestCase
 {
     /**
      * @dataProvider dataProvider
+     *
+     * @param array<string[]> $equations
+     * @param float[]         $values
+     * @param array<string[]> $queries
+     * @param float[]         $result
      */
-    public function test399(int $a, int $result): void
+    public function test399(array $equations, array $values, array $queries, array $result): void
     {
         $sut = new Task399();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->calcEquation($equations, $values, $queries), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[['a', 'aa']], [9.0], [['aa', 'a'], ['aa', 'aa']], [0.1111111111111111, 1.0]],
+            [[['a', 'e'], ['b', 'e']], [4.0, 3.0], [['a', 'b'], ['e', 'e'], ['x', 'x']], [1.3333333333333333, 1.0, -1.0]],
+            [[['a', 'b'], ['b', 'c']], [2.0, 3.0], [['a', 'c'], ['b', 'a'], ['a', 'e'], ['a', 'a'], ['x', 'x']], [6.0, 0.5, -1.0, 1.0, -1.0]],
+            [[['a', 'b'], ['b', 'c'], ['bc', 'cd']], [1.5, 2.5, 5.0], [['a', 'c'], ['c', 'b'], ['bc', 'cd'], ['cd', 'bc']], [3.75, 0.4, 5.0, 0.2]],
+            [[['a', 'b']], [0.5], [['a', 'b'], ['b', 'a'], ['a', 'c'], ['x', 'y']], [0.5, 2.0, -1.0, -1.0]],
         ];
     }
 }
