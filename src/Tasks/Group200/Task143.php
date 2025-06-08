756ce8a920356442af8ecf04768f9357550dc56f
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2025-06-08 23:11:58 +0300

    task 113

--- src/Tasks/Group200/Task143.php
+++ src/Tasks/Group200/Task143.php
@@ -4,9 +4,32 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group200;
 
+use App\Model\ListNode;
+
+/**
+ * 0/100
+ * https://leetcode.com/problems/reorder-list.
+ */
 final class Task143
 {
-    public function execute(): void
+    public function reorderList(ListNode $head): void
     {
+        do {
+            $stack[] = $head;
+            $head = $head->next;
+        } while ($head !== null);
+
+        $result = array_shift($stack);
+        $current = $result;
+
+        $i = true;
+        while (!empty($stack)) {
+            $next = $i ? array_pop($stack) : array_shift($stack);
+            $current->next = $next;
+            $current = $next;
+            $i = !$i;
+        }
+
+        $current->next = null;
     }
 }
--- tests/Group200/Task143Test.php
+++ tests/Group200/Task143Test.php
@@ -4,24 +4,25 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group200;
 
+use App\Model\ListNode;
 use PHPUnit\Framework\TestCase;
 
 final class Task143Test extends TestCase
 {
-    /**
-     * @dataProvider dataProvider
-     */
-    public function test143(int $a, int $result): void
+    public function test143(): void
     {
         $sut = new Task143();
 
-        self::assertSame($a, $result);
-    }
+        $list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
 
-    public static function dataProvider(): array
-    {
-        return [
-            [1, 1],
-        ];
+        $sut->reorderList($list);
+
+        self::assertSame($list->toArray(), [1, 4, 2, 3]);
+
+        $list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+        $sut->reorderList($list);
+
+        self::assertSame($list->toArray(), [1, 5, 2, 4, 3]);
     }
 }
