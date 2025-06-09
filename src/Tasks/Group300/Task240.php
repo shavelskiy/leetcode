5e5626c0fce34114acfda29bceedec85620a6bd6
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2025-06-09 14:09:52 +0300

    task 240

--- src/Tasks/Group300/Task240.php
+++ src/Tasks/Group300/Task240.php
@@ -4,9 +4,33 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group300;
 
+/**
+ * 8.33/66.67.
+ * https://leetcode.com/problems/search-a-2d-matrix-ii/description/?envType=problem-list-v2.
+ */
 final class Task240
 {
-    public function execute(): void
+    /**
+     * @param array<int[]> $matrix
+     */
+    public function searchMatrix(array $matrix, int $target): bool
     {
+        $row = 0;
+        $rows = count($matrix);
+        $col = count($matrix[0]) - 1;
+
+        while ($col >= 0 && $row < $rows) {
+            if ($matrix[$row][$col] === $target) {
+                return true;
+            }
+
+            if ($matrix[$row][$col] > $target) {
+                --$col;
+            } else {
+                ++$row;
+            }
+        }
+
+        return false;
     }
 }
--- tests/Group300/Task240Test.php
+++ tests/Group300/Task240Test.php
@@ -11,17 +11,18 @@ final class Task240Test extends TestCase
     /**
      * @dataProvider dataProvider
      */
-    public function test240(int $a, int $result): void
+    public function test240(array $matrix, int $a, bool $result): void
     {
         $sut = new Task240();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->searchMatrix($matrix, $a), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 5, true],
+            [[[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 20, false],
         ];
     }
 }
