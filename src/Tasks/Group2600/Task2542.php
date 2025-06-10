b3b8bf7cd24b59b594383f7854153beddb72c581
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2025-06-11 02:17:39 +0300

    task 2542

--- src/Tasks/Group2600/Task2542.php
+++ src/Tasks/Group2600/Task2542.php
@@ -4,9 +4,43 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group2600;
 
+use SplMinHeap;
+
+/**
+ * 100/100
+ * https://leetcode.com/problems/maximum-subsequence-score/description/?envType=study-plan-v2.
+ */
 final class Task2542
 {
-    public function execute(): void
+    /**
+     * @param int[] $nums1
+     * @param int[] $nums2
+     */
+    public function maxScore(array $nums1, array $nums2, int $k): int
     {
+        $indices = range(0, count($nums1) - 1);
+
+        usort($indices, fn (int $a, int $b) => $nums2[$b] <=> $nums2[$a]);
+
+        $result = 0;
+        $sum = 0;
+        $heap = new SplMinHeap();
+
+        foreach ($indices as $index) {
+            $n1 = $nums1[$index];
+
+            $heap->insert($n1);
+            $sum += $n1;
+
+            if ($heap->count() > $k) {
+                $sum -= $heap->extract();
+            }
+
+            if ($heap->count() === $k) {
+                $result = max($result, $sum * $nums2[$index]);
+            }
+        }
+
+        return $result;
     }
 }
--- tests/Group2600/Task2542Test.php
+++ tests/Group2600/Task2542Test.php
@@ -11,17 +11,18 @@ final class Task2542Test extends TestCase
     /**
      * @dataProvider dataProvider
      */
-    public function test2542(int $a, int $result): void
+    public function test2542(array $nums1, array $nums2, int $k, int $result): void
     {
         $sut = new Task2542();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->maxScore($nums1, $nums2, $k), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[1, 3, 3, 2], [2, 1, 3, 4], 3, 12],
+            [[4, 2, 3, 1, 1], [7, 5, 10, 9, 6], 1, 30],
         ];
     }
 }
