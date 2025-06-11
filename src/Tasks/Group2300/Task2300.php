3c594942a22c6fe2b486530fdd594b7ba3c0e341
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2025-06-12 00:11:34 +0300

    task 2300

--- src/Tasks/Group2300/Task2300.php
+++ src/Tasks/Group2300/Task2300.php
@@ -4,9 +4,41 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group2300;
 
+/**
+ * 40/100
+ * https://leetcode.com/problems/successful-pairs-of-spells-and-potions.
+ */
 final class Task2300
 {
-    public function execute(): void
+    /**
+     * @param int[] $spells
+     * @param int[] $potions
+     *
+     * @return int[]
+     */
+    public function successfulPairs(array $spells, array $potions, int $success): array
     {
+        sort($potions);
+        $m = count($potions);
+
+        $result = [];
+        foreach ($spells as $spell) {
+            $start = 0;
+            $end = $m - 1;
+
+            while ($start <= $end) {
+                $current = (int)floor(($start + $end) / 2);
+
+                if ($potions[$current] * $spell >= $success) {
+                    $end = $current - 1;
+                } else {
+                    $start = $current + 1;
+                }
+            }
+
+            $result[] = $m - $start;
+        }
+
+        return $result;
     }
 }
--- tests/Group2300/Task2300Test.php
+++ tests/Group2300/Task2300Test.php
@@ -11,17 +11,18 @@ final class Task2300Test extends TestCase
     /**
      * @dataProvider dataProvider
      */
-    public function test2300(int $a, int $result): void
+    public function test2300(array $spells, array $potions, int $success, array $result): void
     {
         $sut = new Task2300();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->successfulPairs($spells, $potions, $success), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[5, 1, 3], [1, 2, 3, 4, 5], 7, [4, 0, 3]],
+            [[3, 1, 2], [8, 5, 8], 16, [2, 0, 2]],
         ];
     }
 }
