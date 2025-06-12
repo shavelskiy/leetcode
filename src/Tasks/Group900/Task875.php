52dcca9d910ff21967f1d5a633f245f43e8ea05c
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2025-06-13 00:48:29 +0300

    task 875

--- src/Tasks/Group900/Task875.php
+++ src/Tasks/Group900/Task875.php
@@ -4,9 +4,34 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group900;
 
+/**
+ * https://leetcode.com/problems/koko-eating-bananas.
+ */
 final class Task875
 {
-    public function execute(): void
+    /**
+     * @param int[] $piles
+     */
+    public function minEatingSpeed(array $piles, int $h): int
     {
+        $left = 1;
+        $right = max($piles);
+
+        while ($left <= $right) {
+            $middle = intdiv($left + $right, 2);
+
+            $current = 0;
+            foreach ($piles as $pile) {
+                $current += ceil($pile / $middle);
+            }
+
+            if ((int)$current > $h) {
+                $left = $middle + 1;
+            } else {
+                $right = $middle - 1;
+            }
+        }
+
+        return $left;
     }
 }
--- tests/Group900/Task875Test.php
+++ tests/Group900/Task875Test.php
@@ -11,17 +11,20 @@ final class Task875Test extends TestCase
     /**
      * @dataProvider dataProvider
      */
-    public function test875(int $a, int $result): void
+    public function test875(array $piles, int $h, int $result): void
     {
         $sut = new Task875();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->minEatingSpeed($piles, $h, $result), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
-            [1, 1],
+            [[999], 10, 100],
+            [[3, 6, 7, 11], 8, 4],
+            [[30, 11, 23, 4, 20], 5, 30],
+            [[30, 11, 23, 4, 20], 6, 23],
         ];
     }
 }
