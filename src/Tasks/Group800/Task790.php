6785fcb66efa44cd723b1654851321aae8390339
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2025-06-13 21:10:45 +0300

    task 790

--- src/Tasks/Group800/Task790.php
+++ src/Tasks/Group800/Task790.php
@@ -4,9 +4,25 @@ declare(strict_types=1);
 
 namespace App\Tasks\Group800;
 
+/**
+ * https://leetcode.com/problems/domino-and-tromino-tiling.
+ */
 final class Task790
 {
-    public function execute(): void
+    public function numTilings(int $n): int
     {
+        $mod = 1e9 + 7;
+
+        $map = [1, 2, 1];
+
+        for ($i = 1; $i < $n; ++$i) {
+            $map = [
+                ($map[0] + $map[2]) % $mod,
+                ($map[0] + $map[0] + $map[1]) % $mod,
+                ($map[0] + $map[1]) % $mod,
+            ];
+        }
+
+        return $map[0];
     }
 }
--- tests/Group800/Task790Test.php
+++ tests/Group800/Task790Test.php
@@ -11,16 +11,19 @@ final class Task790Test extends TestCase
     /**
      * @dataProvider dataProvider
      */
-    public function test790(int $a, int $result): void
+    public function test790(int $n, int $result): void
     {
         $sut = new Task790();
 
-        self::assertSame($a, $result);
+        self::assertSame($sut->numTilings($n), $result);
     }
 
     public static function dataProvider(): array
     {
         return [
+            [60, 882347204],
+            [28, 914332884],
+            [3, 5],
             [1, 1],
         ];
     }
